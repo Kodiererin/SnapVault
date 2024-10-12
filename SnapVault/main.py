430a3eba472df836680f1dd6127a7617763d12e6
Kodiererin <iamujjwal45@gmail.com>
2024-10-13 02:54:01 +0530

    (Feature) : Testing the Libraries

--- SnapVault/main.py
+++ SnapVault/main.py
@@ -1,39 +1,32 @@
-# 1. Install the libraries
-# 2. Recognize the environment
-# 3. Recognize the face.
-# 4. Create the output folder and save the recognized face.
-
-# Lets do it!!!!
-
 import face_recognition
 import os
 import shutil
 
-
 input_folder = 'images'
 output_folder = 'output'
 
-# Creating an array to store the faces
-
+# Array to store known faces and folders for each face
 facesKnown = []
 faceFolder = []
 
 def imageProcessing():
-    facesKnown = []
-    faceFolder = []
+    global facesKnown, faceFolder
 
+    # Iterate over each image in the folder
     for img in os.listdir(input_folder):
         imagePath = os.path.join(input_folder, img)
 
         print("Processing Image: ", imagePath)
 
-        # Lets Load the image!
+        # Load the image
         image = face_recognition.load_image_file(imagePath)
 
-        faceLoacation = face_recognition.face_locations(image)
-        faceEncoding = face_recognition.face_encodings(image, faceLoacation)
+        # Find all face locations and encodings in the image
+        faceLocations = face_recognition.face_locations(image)
+        faceEncodings = face_recognition.face_encodings(image, faceLocations)
 
-
+       
+# Start the process
 print("Welcome to SnapVault") 
-print("Lets start recognizing the faces")
+print("Let's start recognizing the faces")
 imageProcessing()
